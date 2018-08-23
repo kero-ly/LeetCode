46bce9e10349a5c40d64fba8d6e7df69f7d61e98
keroly <943355102@qq.com>
2018-08-23 13:39:51 +0800

    1_两数之和

--- Algorithm/1_sum_of_two_nums.cpp
+++ Algorithm/1_sum_of_two_nums.cpp
@@ -1,6 +1,6 @@
-/*
+/*  '''
+【题目】
 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
-
 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
 
 示例:
@@ -8,8 +8,39 @@
 给定 nums = [2, 7, 11, 15], target = 9
 因为 nums[0] + nums[1] = 2 + 7 = 9
 所以返回 [0, 1]
-*/
+'''  */
 
+/*
+解法1
+双重循环遍历，
+时间复杂度O(n^2)
+空间复杂度O(1)
+最暴力的方法，简单，但耗费资源
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (unsigned int i = 0; i < nums.size(); i++) {
+            for (unsigned int j = i+1; j < nums.size(); j++) {
+                if ((target - nums[i]) == nums[j]) {
+                    vector<int> ret = {i, j};
+                    return ret;
+                }
+            }
+        }
+        return {};
+    }
+};
+
+/*
+解法1
+哈希表（散列表）Hash table
+时间复杂度O(n)
+空间复杂度O(n)
+通过以空间换取速度的方式，我们可以将查找时间从O(n)降低到O(1)。
+哈希表正是为此目的而构建的，它支持以 "近似" 恒定的时间进行快速查找
+为什么说近似恒定？如果出现数据冲突，则查找时间会从O(1)升低到O(n)，例如（K1!=K2，但f(K1)=f(K2)）
+*/
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
